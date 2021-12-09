bfa772f457dfe3800a08627caf5eccf1fa58b24d
Ermolaeff <egorka.ermolaev.02@gmail.com>
2021-12-09 18:58:13 +0300

    Автопрокат 1

---
+++ Avtoprokat/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Avtoprokat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowModelPage(object sender, RoutedEventArgs e)
+        {
+            AvtoprokatFrame.Navigate(new Pages.ModelPage());
+        }
+
+        private void ShowMakePage(object sender, RoutedEventArgs e)
+        {
+            AvtoprokatFrame.Navigate(new Pages.MakePage());
+        }
+
+        private void AddElement(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteElement(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
